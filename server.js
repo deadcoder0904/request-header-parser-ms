f51559ccb5f0e196f2f228c25d4efc130a8eb930
Akshay Kadam <akshaykadam0904@gmail.com>
2016-07-03 22:59:16 +0530

    Final Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var open = require("open");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/api/whoami',function(req,res) {
+	var headers = req.headers;
+	var ip = req.ip.split(':').pop();
+	var language = headers["accept-language"].split(';').shift();
+	var software = headers["user-agent"].match(/\((.*?)\)/)[1];
+
+	res.json({
+		"ipaddress": ip,
+		"language":  language,
+		"software":  software
+	});
+});
+
+app.listen(port,function() {
+	var url = "http://localhost:"+port;
+	console.log("MAGIC HAPPENING @ " + url);
+	open(url + "/api/whoami");
+});
